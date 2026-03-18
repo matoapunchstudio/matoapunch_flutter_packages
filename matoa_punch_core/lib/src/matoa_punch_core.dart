ceae1f9778d868a9c09ee6cbcb7894c3739fdc00
wandypurnomo <wandypurnomo92@gmail.com>
2026-03-18 10:13:06 +0700

    initial commit

---
+++ matoa_punch_core/lib/src/matoa_punch_core.dart
@@ -0,0 +1,7 @@
+/// {@template matoa_punch_core}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class MatoaPunchCore {
+  /// {@macro matoa_punch_core}
+  const MatoaPunchCore();
+}
---
+++ matoa_punch_core/test/src/matoa_punch_core_test.dart
@@ -0,0 +1,13 @@
+// Not required for test files
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:matoa_punch_core/matoa_punch_core.dart';
+
+void main() {
+  group('MatoaPunchCore', () {
+    test('can be instantiated', () {
+      expect(MatoaPunchCore(), isNotNull);
+    });
+  });
+}
